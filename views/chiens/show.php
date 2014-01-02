ec7c50a647ea2183f4803820577787a9791bac66
Stocks Oliver <stocks.oliver@gmail.com>
2014-01-02 12:33:22 +0100

    use the image table for the dogs carrousel

--- views/chiens/show.php
+++ views/chiens/show.php
@@ -14,42 +14,9 @@
 	<div class="row">
 		<div class="col-lg-7" style="padding-top: 15px;">
 
-<div id="carousel-example-generic" class="carousel slide" style="border: 1px solid #bdc3c7;" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="6"></li>
-  </ol>
+   		<?php $helper::carrousel($dog_by_id->dogimages); ?>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner">
-    <div class="item active">
-      <img src="/public/images/8176746583_d352e78650_c.jpg" alt="First image">
-      <div class="carousel-caption">
-        Dog picture 
-      </div>
-    </div>
-    <?php 
-      foreach($dog_by_id->dogimages as $image)  {
-       echo '<div class="item"><img src="'. $image->filename .'" /><div class="carousel-caption">Dog</div></div>';
-    }
-    ?>
-   </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-  </a>
-</div>
- <br />
+		 <br />
 			<?php echo $dog_by_id->textdog ?>
 		</div>
 		<div class="col-lg-5">
