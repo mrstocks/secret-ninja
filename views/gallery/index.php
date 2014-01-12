5b4d2f7b9caa2d0fdb9aa230d076df607d62acc6
Acho Arnold <acho.arnold@live.com>
2014-01-11 20:11:37 -0800

    Hope You love the design

--- views/gallery/index.php
+++ views/gallery/index.php
@@ -1,20 +1,20 @@
 <?php
 	/* To Do
 	- style the various tiles
-	- Create tag which displays if dog is available and a link to info about the dog
-   - display the see more button on hover
+	- create a fade out css effect
    */
 ?>
 
-	<script src="../public/js/masonry.pkgd.min.js"></script>
+	<script src="/public/js/masonry.pkgd.min.js"></script>
 	<div class="row">
 	<h3>Welcome to our Gallery</h3>
 	<p>Below are some of the great views which we have captured on film (or digital memory!)<br/>
 		Click on any link below to find out more.</p>
-		<div class="gallery">
+		<div class="gallery" >
 			<div class="col-md-12">
 				<div class="js-masonry"  data-masonry-options='{ "columnWidth": 10, "itemSelector": ".gallery-tile" }'>
-				  	<?php helper::gallery_tile($gallerysections); ?>
+				  	<?php  Helper::gallery_tile($gallerysections); ?>
+				  	</div>
 				</div>
 			</div>
 		</div>
