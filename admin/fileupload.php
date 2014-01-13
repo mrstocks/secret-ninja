bba0750d11685a73e5294cb7a3fdd978acd8359d
Stocks Oliver <stocks.oliver@gmail.com>
2014-01-13 19:23:33 +0100

    Admin uploads for the galleries

--- admin/fileupload.php
+++ admin/fileupload.php
@@ -1,17 +1,21 @@
 <?php
+require_once __DIR__ . "/config/config.php"; 
+require_once __DIR__ . "/models/init.php";
+
+
 $ds          = DIRECTORY_SEPARATOR;  //1
  
-$storeFolder = '/opt/secret-ninja/public/images/';   //2
+$storeFolder = '/opt/secret-ninja/public/images/galleries/';   //2
  
 if (!empty($_FILES)) {
      
-    $tempFile = $_FILES['file']['tmp_name'];          //3             
-      
-    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
-     
-    $targetFile =  $targetPath. $_FILES['file']['name'];  //5
- 
-    move_uploaded_file($tempFile,$targetFile); //6
-     
+	$tempFile = $_FILES['file']['tmp_name'];          //3             
+	$targetFile =  $storeFolder. $_FILES['file']['name'];  //5	
+	
+	move_uploaded_file($tempFile,$targetFile);
+	$gallery = New Gallery();
+	$gallery->filename = "/public/images/galleries/".$_FILES['file']['name'];	
+	$gallery->save();
+
 }
 ?>     
--- admin/models/Dog.php
+++ admin/models/Dog.php
@@ -1,12 +1,12 @@
-<?php 
-/** 
+<?php
+/**
  * Website errors in the backend i want to log them
  *
  */
 
-class Dog extends ActiveRecord\Model { 
+class Dog extends ActiveRecord\Model {
 	static $has_many = array(
 		array('dogimages')
 		);
-} 
+}
 ?>
