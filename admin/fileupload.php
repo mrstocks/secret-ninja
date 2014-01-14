46e72845a6671581356274901f86a934215a5f7d
Stocks Oliver <stocks.oliver@gmail.com>
2014-01-14 21:42:36 +0100

    admin for the gallery

--- admin/fileupload.php
+++ admin/fileupload.php
@@ -1,21 +1,26 @@
 <?php
 require_once __DIR__ . "/config/config.php"; 
 require_once __DIR__ . "/models/init.php";
+require_once __DIR__ . "/helpers/resize-class.php";
 
-
-$ds          = DIRECTORY_SEPARATOR;  //1
+$ds          = DIRECTORY_SEPARATOR;  
  
-$storeFolder = '/opt/secret-ninja/public/images/galleries/';   //2
+$storeFolder = '/opt/secret-ninja/public/images/galleries/';
  
 if (!empty($_FILES)) {
      
-	$tempFile = $_FILES['file']['tmp_name'];          //3             
-	$targetFile =  $storeFolder. $_FILES['file']['name'];  //5	
+	$tempFile = $_FILES['file']['tmp_name'];                      
+	$targetFile =  $storeFolder. $_FILES['file']['name']; 
 	
 	move_uploaded_file($tempFile,$targetFile);
+	
+	$resizeObj = new resize($targetFile);
+	$resizeObj->resizeImage(200, 200, 'crop');
+	$resizeObj->saveImage($targetFile . "-resized.jpg", 150);
+
 	$gallery = New Gallery();
 	$gallery->filename = "/public/images/galleries/".$_FILES['file']['name'];	
+	$gallery->thumb = "/public/images/galleries/".$_FILES['file']['name'] . "-resized.jpg";
 	$gallery->save();
-
 }
 ?>     
--- admin/galleryadmin.php
+++ admin/galleryadmin.php
@@ -1,8 +1,33 @@
+<?php
+
+require_once __DIR__ . "/config/config.php"; 
+require_once __DIR__ . "/models/init.php";
+
+// Every file will need this.
+session_start(); 
+
+if (!isset($_SESSION['logged_in']))  {
+	$error = Errors::create(array(
+		'level'	=> '2', 
+		'title'	=> "Security information.",
+		'info'	=> "Someone tried to login with a index but wasn't authentitacted."
+	));
+
+	header("Location: http://$host$uri/login.php");
+	exit;
+}
+
+$title = "Celestia Admin";
+$subtitle = "Gallery admin";
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
-		<title>Blank Page - Ace Admin</title>
+		<title><?php echo $subtitle ." - ".  $title ?></title>
 
 		<meta name="description" content="" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -57,260 +82,11 @@
 					<a href="#" class="navbar-brand">
 						<small>
 							<i class="icon-leaf"></i>
-							Ace Admin
+							Feed de Celestia - Admin
 						</small>
 					</a><!-- /.brand -->
 				</div><!-- /.navbar-header -->
-
-				<div class="navbar-header pull-right" role="navigation">
-					<ul class="nav ace-nav">
-						<li class="grey">
-							<a data-toggle="dropdown" class="dropdown-toggle" href="#">
-								<i class="icon-tasks"></i>
-								<span class="badge badge-grey">4</span>
-							</a>
-
-							<ul class="pull-right dropdown-navbar dropdown-menu dropdown-caret dropdown-close">
-								<li class="dropdown-header">
-									<i class="icon-ok"></i>
-									4 Tasks to complete
-								</li>
-
-								<li>
-									<a href="#">
-										<div class="clearfix">
-											<span class="pull-left">Software Update</span>
-											<span class="pull-right">65%</span>
-										</div>
-
-										<div class="progress progress-mini ">
-											<div style="width:65%" class="progress-bar "></div>
-										</div>
-									</a>
-								</li>
-
-								<li>
-									<a href="#">
-										<div class="clearfix">
-											<span class="pull-left">Hardware Upgrade</span>
-											<span class="pull-right">35%</span>
-										</div>
-
-										<div class="progress progress-mini ">
-											<div style="width:35%" class="progress-bar progress-bar-danger"></div>
-										</div>
-									</a>
-								</li>
-
-								<li>
-									<a href="#">
-										<div class="clearfix">
-											<span class="pull-left">Unit Testing</span>
-											<span class="pull-right">15%</span>
-										</div>
-
-										<div class="progress progress-mini ">
-											<div style="width:15%" class="progress-bar progress-bar-warning"></div>
-										</div>
-									</a>
-								</li>
-
-								<li>
-									<a href="#">
-										<div class="clearfix">
-											<span class="pull-left">Bug Fixes</span>
-											<span class="pull-right">90%</span>
-										</div>
-
-										<div class="progress progress-mini progress-striped active">
-											<div style="width:90%" class="progress-bar progress-bar-success"></div>
-										</div>
-									</a>
-								</li>
-
-								<li>
-									<a href="#">
-										See tasks with details
-										<i class="icon-arrow-right"></i>
-									</a>
-								</li>
-							</ul>
-						</li>
-
-						<li class="purple">
-							<a data-toggle="dropdown" class="dropdown-toggle" href="#">
-								<i class="icon-bell-alt icon-animated-bell"></i>
-								<span class="badge badge-important">8</span>
-							</a>
-
-							<ul class="pull-right dropdown-navbar navbar-pink dropdown-menu dropdown-caret dropdown-close">
-								<li class="dropdown-header">
-									<i class="icon-warning-sign"></i>
-									8 Notifications
-								</li>
-
-								<li>
-									<a href="#">
-										<div class="clearfix">
-											<span class="pull-left">
-												<i class="btn btn-xs no-hover btn-pink icon-comment"></i>
-												New Comments
-											</span>
-											<span class="pull-right badge badge-info">+12</span>
-										</div>
-									</a>
-								</li>
-
-								<li>
-									<a href="#">
-										<i class="btn btn-xs btn-primary icon-user"></i>
-										Bob just signed up as an editor ...
-									</a>
-								</li>
-
-								<li>
-									<a href="#">
-										<div class="clearfix">
-											<span class="pull-left">
-												<i class="btn btn-xs no-hover btn-success icon-shopping-cart"></i>
-												New Orders
-											</span>
-											<span class="pull-right badge badge-success">+8</span>
-										</div>
-									</a>
-								</li>
-
-								<li>
-									<a href="#">
-										<div class="clearfix">
-											<span class="pull-left">
-												<i class="btn btn-xs no-hover btn-info icon-twitter"></i>
-												Followers
-											</span>
-											<span class="pull-right badge badge-info">+11</span>
-										</div>
-									</a>
-								</li>
-
-								<li>
-									<a href="#">
-										See all notifications
-										<i class="icon-arrow-right"></i>
-									</a>
-								</li>
-							</ul>
-						</li>
-
-						<li class="green">
-							<a data-toggle="dropdown" class="dropdown-toggle" href="#">
-								<i class="icon-envelope icon-animated-vertical"></i>
-								<span class="badge badge-success">5</span>
-							</a>
-
-							<ul class="pull-right dropdown-navbar dropdown-menu dropdown-caret dropdown-close">
-								<li class="dropdown-header">
-									<i class="icon-envelope-alt"></i>
-									5 Messages
-								</li>
-
-								<li>
-									<a href="#">
-										<img src="assets/avatars/avatar.png" class="msg-photo" alt="Alex's Avatar" />
-										<span class="msg-body">
-											<span class="msg-title">
-												<span class="blue">Alex:</span>
-												Ciao sociis natoque penatibus et auctor ...
-											</span>
-
-											<span class="msg-time">
-												<i class="icon-time"></i>
-												<span>a moment ago</span>
-											</span>
-										</span>
-									</a>
-								</li>
-
-								<li>
-									<a href="#">
-										<img src="assets/avatars/avatar3.png" class="msg-photo" alt="Susan's Avatar" />
-										<span class="msg-body">
-											<span class="msg-title">
-												<span class="blue">Susan:</span>
-												Vestibulum id ligula porta felis euismod ...
-											</span>
-
-											<span class="msg-time">
-												<i class="icon-time"></i>
-												<span>20 minutes ago</span>
-											</span>
-										</span>
-									</a>
-								</li>
-
-								<li>
-									<a href="#">
-										<img src="assets/avatars/avatar4.png" class="msg-photo" alt="Bob's Avatar" />
-										<span class="msg-body">
-											<span class="msg-title">
-												<span class="blue">Bob:</span>
-												Nullam quis risus eget urna mollis ornare ...
-											</span>
-
-											<span class="msg-time">
-												<i class="icon-time"></i>
-												<span>3:15 pm</span>
-											</span>
-										</span>
-									</a>
-								</li>
-
-								<li>
-									<a href="inbox.html">
-										See all messages
-										<i class="icon-arrow-right"></i>
-									</a>
-								</li>
-							</ul>
-						</li>
-
-						<li class="light-blue">
-							<a data-toggle="dropdown" href="#" class="dropdown-toggle">
-								<img class="nav-user-photo" src="assets/avatars/user.jpg" alt="Jason's Photo" />
-								<span class="user-info">
-									<small>Welcome,</small>
-									Jason
-								</span>
-
-								<i class="icon-caret-down"></i>
-							</a>
-
-							<ul class="user-menu pull-right dropdown-menu dropdown-yellow dropdown-caret dropdown-close">
-								<li>
-									<a href="#">
-										<i class="icon-cog"></i>
-										Settings
-									</a>
-								</li>
-
-								<li>
-									<a href="#">
-										<i class="icon-user"></i>
-										Profile
-									</a>
-								</li>
-
-								<li class="divider"></li>
-
-								<li>
-									<a href="#">
-										<i class="icon-off"></i>
-										Logout
-									</a>
-								</li>
-							</ul>
-						</li>
-					</ul><!-- /.ace-nav -->
-				</div><!-- /.navbar-header -->
+				<?php include __DIR__ . "/partial/nav-bar-header.php"; ?>
 			</div><!-- /.container -->
 		</div>
 
@@ -358,10 +134,8 @@
 							<span class="btn btn-danger"></span>
 						</div>
 					</div><!-- #sidebar-shortcuts -->
+					<?php include "menu.php"; ?>
 
-
-
-					<?php include menu.php ?>
 					<div class="sidebar-collapse" id="sidebar-collapse">
 						<i class="icon-double-angle-left" data-icon1="icon-double-angle-left" data-icon2="icon-double-angle-right"></i>
 					</div>
@@ -384,9 +158,9 @@
 							</li>
 
 							<li>
-								<a href="#">Other Pages</a>
+								<a href="#">Gallery</a>
 							</li>
-							<li class="active">Blank Page</li>
+							<li class="active">Gallery Admin</li>
 						</ul><!-- .breadcrumb -->
 
 						<div class="nav-search" id="nav-search">
@@ -400,10 +174,75 @@
 					</div>
 
 					<div class="page-content">
+						<div class="page-header">
+							<h1>Admin <small><i class="icon-double-angle-right"></i> The gallery</small></h1>
+						</div>
 						<div class="row">
 							<div class="col-xs-12">
 								<!-- PAGE CONTENT BEGINS -->
+								<form class="form-horizontal" role="form" action="updategallery.php">
+								<?php 
+									$galleries = Gallery::all();
 
+									foreach ($galleries as $gallery) {
+								?>
+
+									<div class="form-group">
+										
+										<div class="col-sm-3 control-label no-padding-right">
+										</div>
+										<div class="col-sm-9">
+										<img src="<?php echo $gallery->thumb; ?>" />
+										</div>
+									</div>								
+	
+									<div class="space-4"></div>
+
+									<div class="form-group">
+								
+										<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> Title </label>
+
+										<div class="col-sm-9">
+											<input type="text" id="form-field-1" placeholder="Username" class="col-xs-10 col-sm-5" />
+										</div>
+									</div>
+
+									<div class="space-4"></div>
+
+									<div class="form-group">
+										<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> </label>
+
+										<div class="col-sm-4">
+
+											<div class="widget-box">
+												<div class="widget-header">
+													<h4>Dispo?</h4>
+
+												</div>
+
+												<div class="widget-body">
+													<div class="widget-main">
+														<div>
+
+															<select class="form-control" id="form-field-select-1">
+																<option value="">&nbsp;</option>
+																<option value="0">Vendu</option>
+																<option value="1">Reste a l'elevage</option>
+																<option value="2">Disponnible</option>
+															</select>
+														</div>
+
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<hr />
+									<?php
+									} 
+									?>
+								</div>
+					
 								<!-- PAGE CONTENT ENDS -->
 							</div><!-- /.col -->
 						</div><!-- /.row -->
