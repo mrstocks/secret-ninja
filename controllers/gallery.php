8f3e475600a678ec50350b306068279ecc3d7311
najela <acho.arnold@live.com>
2014-01-15 01:19:58 +0100

    Solved all the issues

--- controllers/gallery.php
+++ controllers/gallery.php
@@ -1,6 +1,6 @@
 <?php
 /***
- * TTO DO
+ * TO DO
  * - Create mechanism to ensure that different pictures are displayed on each tile
  * 
  */
--- views/gallery/index.php
+++ views/gallery/index.php
@@ -7,15 +7,16 @@
 	<div class="container">
 	<script src="/public/js/masonry.pkgd.min.js"></script>
 	<div class="row">
-	<h3>Welcome to our Gallery</h3>
-	<p>Below are some of the great views which we have captured on film (or digital memory!)<br/>
-		Click on any link below to find out more.</p>
-		<div class="gallery" >
-			<div class="col-md-12">
-				<div class="js-masonry"  data-masonry-options='{ "columnWidth": 10, "itemSelector": ".gallery-tile" }'>
-				  	<?php  Helper::gallery_tile($galleries); ?>
-				  	</div>
-				</div>
+	<h3>Notre Galerie</h3>
+	<p>Voici quelques-uns des grands points de vue que nous avons capturés sur le film (ou la mémoire numérique!)<br />
+			Cliquez sur un chien disponible pour en savoir plus.
+	</p>
+	<div class="gallery" >
+		<div class="col-md-12">
+			<div class="js-masonry"  data-masonry-options='{ "columnWidth": 10, "itemSelector": ".gallery-tile" }'>
+			  	<?php  Helper::gallery_tile($galleries); ?>
+			  	</div>
 			</div>
 		</div>
 	</div>
+</div>
