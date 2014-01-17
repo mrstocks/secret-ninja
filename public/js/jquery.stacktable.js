76d6ae7fffecbb52391a44ee797f78cba72727d3
najela <acho.arnold@live.com>
2014-01-18 00:17:20 +0100

    Started the bienvenu interface

--- public/js/jquery.stacktable.js
+++ public/js/jquery.stacktable.js
@@ -10,45 +10,49 @@
  * jQuery plugin for stacking tables on small screens
  *
  */
-;(function($) {
+;
+(function($) {
 
-  $.fn.stacktable = function(options) {
-    var $tables = this,
-        defaults = {id:'stacktable',hideOriginal:false},
-        settings = $.extend({}, defaults, options),
-        stacktable;
+    $.fn.stacktable = function(options) {
+        var $tables = this,
+            defaults = {
+                id: 'stacktable',
+                hideOriginal: false
+            },
+            settings = $.extend({}, defaults, options),
+            stacktable;
 
-    return $tables.each(function() {
-      var $stacktable = $('<table class="'+settings.id+'"><tbody></tbody></table>');
-      if (typeof settings.myClass !== undefined) $stacktable.addClass(settings.myClass);
-      var markup = '';
-      $table = $(this);
-      $topRow = $table.find('tr').eq(0);
-      $table.find('tr').each(function(index,value) {
-        var zebra = "";
-        if (index % 2 === 0) {
-          zebra = "even";
-        } else {
-          zebra = "odd";
-        }
-        markup += '<tr class="' + zebra + '">';
-        $(this).find('td').each(function(index,value) {
-          if ($(this).html() !== ''){
-            markup += '<tr class="' + zebra + '">';
-            if ($topRow.find('td,th').eq(index).html()){
-              markup += '<td>'+$topRow.find('td,th').eq(index).html()+'</td>';
-            } else {
-              markup += '<td></td>';
-            }
-            markup += '<td>'+$(this).html()+'</td>';
-            markup += '</tr>';
-          }
+        return $tables.each(function() {
+            var $stacktable = $('<table class="' + settings.id + '"><tbody></tbody></table>');
+            if (typeof settings.myClass !== undefined) $stacktable.addClass(settings.myClass);
+            var markup = '';
+            $table = $(this);
+            $topRow = $table.find('tr').eq(0);
+            $table.find('tr').each(function(index, value) {
+                var zebra = "";
+                if (index % 2 === 0) {
+                    zebra = "even";
+                } else {
+                    zebra = "odd";
+                }
+                markup += '<tr class="' + zebra + '">';
+                $(this).find('td').each(function(index, value) {
+                    if ($(this).html() !== '') {
+                        markup += '<tr class="' + zebra + '">';
+                        if ($topRow.find('td,th').eq(index).html()) {
+                            markup += '<td>' + $topRow.find('td,th').eq(index).html() + '</td>';
+                        } else {
+                            markup += '<td></td>';
+                        }
+                        markup += '<td>' + $(this).html() + '</td>';
+                        markup += '</tr>';
+                    }
+                });
+            });
+            $stacktable.append($(markup));
+            $table.before($stacktable);
+            if (settings.hideOriginal) $table.hide();
         });
-      });
-      $stacktable.append($(markup));
-      $table.before($stacktable);
-      if (settings.hideOriginal) $table.hide();
-    });
-  };
+    };
 
 }(jQuery));
--- views/bienvenu/index.php
+++ views/bienvenu/index.php
@@ -1,41 +1,73 @@
-	<div class="row" style="">
+        <div class="row" style="">
             <div class="col-md-5">
-              <?php require __DIR__ ."/../shared/stickynews.php"; ?>
-              <br />
-	      <p>Plus de nouvelles</p>
-              <?php require __DIR__ ."/../shared/news.php"; ?>
+                <?php require __DIR__ . "/../shared/stickynews.php"; ?>
+                <br />
+                <p>Plus de nouvelles</p>
+                <?php require __DIR__ . "/../shared/news.php"; ?>
             </div>
             <div class="col-md-7">
-		<?php helper::carrousel($carrousel); ?>
-           </div>
-      </div>
+                <?php helper::carrousel($carrousel); ?>
+            </div>
+        </div>
 
 
-      <hr style="" />
-      <div class="row">
+        <hr style="" />
+        <div class="row">
+		<h3>Nouveautés</h3>
+            <div class="col-md-12">
 
-     <div class="col-md-7" style="padding-top: 115px;">
-     <!--[if !IE]> -->
-        <video class="video-js" controls preload="auto" width="630" height="349" poster="/public/images/video/poster.jpg" data-setup="{}">
-          <source src="http://iurevych.github.com/Flat-UI-videos/big_buck_bunny.mp4" type="video/mp4">
-          <source src="http://iurevych.github.com/Flat-UI-videos/big_buck_bunny.webm" type="video/webm">
-        </video>
-      <!-- <![endif]-->
+                <article class="col-md-4 featured-post">
+			  <div class="post-image">
+                    <h5>Finouka</h5> 
+                    <img src="/public/images/8176795608_dd03519ded_c.jpg" alt="desc" style="width:100%" />
+                    </div>
+                    <div clas="post-caption">
+                        <p>Vivamus tortor tellus, rutrum sit amet mollis vel, imperdiet consectetur orci. Mauris pharetra congue enim, et sagittis lectus congue ut. Cum sociis natoque penatibus.</p>
+                    </div>
+                </article>
+                <article class="col-md-4 featured-post">
+			  <div class="post-image">
+                    <h5>Finouka</h5> 
+                    <img src="/public/images/8176795608_dd03519ded_c.jpg" alt="desc" style="width:100%" />
+                    </div>
+                    <div clas="post-caption">
+                        <p>Vivamus tortor tellus, rutrum sit amet mollis vel, imperdiet consectetur orci. Mauris pharetra congue enim, et sagittis lectus congue ut. Cum sociis natoque penatibus.</p>
+                    </div>
+                </article>
+                <article class="col-md-4 featured-post">
+			  <div class="post-image">
+                    <h5>Finouka</h5> 
+                    <img src="/public/images/8176795608_dd03519ded_c.jpg" alt="desc" style="width:100%" />
+                    </div>
+                    <div clas="post-caption">
+                        <p>Vivamus tortor tellus, rutrum sit amet mollis vel, imperdiet consectetur orci. Mauris pharetra congue enim, et sagittis lectus congue ut. Cum sociis natoque penatibus.</p>
+                    </div>
+                </article>
 
-      <!--[if IE]>
-        <video class="video-js" controls preload="auto" width="630" height="256" poster="http://video-js.zencoder.com/oceans-clip.jpg" data-setup="{}">
-          <source src="http://video-js.zencoder.com/oceans-clip.mp4" type='video/mp4'/>
-          <source src="http://video-js.zencoder.com/oceans-clip.webm" type='video/webm'/>
-        </video>
-      <![endif]-->
+            </div>
+        </div>
 
-      </div>
-        	<div class="col-md-5">
-	        	<h5>Elevage de Dogues Allemand!</h5>
-	         	<p>Bonjour ou bonne nuit a vous visiteurs sur le site Fees de Celestia, nous sommes de petit elevage de <span>dogue allemand</span> dans la compagne tres profonde a mayenne.  
-	          	</p>
-       	</div>
-      </div>
+        <hr style="" />
+        <div class="row">
+            <div class="col-md-7" style="padding-top: 115px;">
+                <!--[if !IE]> -->
+                <video class="video-js" controls preload="auto" width="630" height="349" poster="/public/images/video/poster.jpg" data-setup="{}">
+                    <source src="http://iurevych.github.com/Flat-UI-videos/big_buck_bunny.mp4" type="video/mp4">
+                        <source src="http://iurevych.github.com/Flat-UI-videos/big_buck_bunny.webm" type="video/webm">
+                </video>
+                <!-- <![endif]-->
 
+                <!--[if IE]>
+                <video class="video-js" controls preload="auto" width="630" height="256" poster="http://video-js.zencoder.com/oceans-clip.jpg" data-setup="{}">
+                  <source src="http://video-js.zencoder.com/oceans-clip.mp4" type='video/mp4'/>
+                  <source src="http://video-js.zencoder.com/oceans-clip.webm" type='video/webm'/>
+                </video>
+              <![endif]-->
 
-
+            </div>
+            <div class="col-md-5">
+                <h5>Elevage de Dogues Allemand!</h5>
+                <p>Bonjour ou bonne nuit a vous visiteurs sur le site Fees de Celestia, nous sommes de petit elevage de <span>dogue allemand</span> dans la compagne tres profonde a mayenne.
+                </p>
+            </div>
+        </div>
