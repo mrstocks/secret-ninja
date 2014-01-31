51c70ee453a199b58031829814ad7ca8a3666fb2
najela <acho.arnold@live.com>
2014-02-01 00:24:01 +0100

    Some little changes

--- views/shared/menu.php
+++ views/shared/menu.php
@@ -1,21 +1,27 @@
-<div id="header"></div>  
- 
-  <div class="jumbotron">
-  <div class="container">
-    <div class="nav-collapse">
-        <ul class="nav nav-pills pull-right">
-            <li><a href="/index.php"><i class="icon-home icon-2x"></i> Accueil<br></a>
-            </li>
-            <li><a href="/index.php/chiens"><i class=" icon-pencil icon-2x"></i>Chiens</a>
-            </li>
-            <li><a href="/index.php/chiots"><i class=" icon-briefcase icon-2x"></i>Chiot</a>
-            </li>
-            <li><a href="/index.php/gallery"><i class=" icon-envelope icon-2x"></i>Gallerie</a>
-            </li>
-            <li><a href="/index.php/contact"><i class=" icon-envelope icon-2x"></i>Contact</a>
-        </ul>
-         </div>
-       </div>
-
-     </div>
-</div>
+<div id="header"></div>
+<header>
+    		<nav class="navbar navbar-inverse" role="navigation">
+  		<div class="container">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="/index.php">Fees de Celestia</a>
+		    </div>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav nav-pills pull-right">
+			            <li><a href="/index.php">Accueil<br></a></li>
+			            <li><a href="/index.php/chiens">Chiens</a></li>
+			            <li><a href="/index.php/chiots">Chiots</a></li>
+			            <li><a href="/index.php/gallery">Gallerie</a></li>
+			            <li><a href="/index.php/contact">Contact</a></li>
+		        	</ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-->
+		</nav>
+</header>
