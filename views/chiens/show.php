faaa20be5cd64824ed0a775734748e7e24e50d6d
Stocks Oliver <stocks.oliver@gmail.com>
2014-02-04 02:43:09 +0100

    let is breath

--- views/chiens/show.php
+++ views/chiens/show.php
@@ -9,14 +9,16 @@
 
 	<div class="row">
 		<div class="col-lg-7" style="padding-top: 15px;">
-			<dig  class="dog-carrousel">
+			<div  class="dog-carrousel">
    			<?php $helper::carrousel($dog_by_id->dogimages, "dogimage"); ?>
-   		</dig>
-
+   			</div>
    		<div class="dog-info">
-   		<h3>Plus d'informations</h3>
-			<?php echo $dog_by_id->textdog ?>
-			</div>
+		<section class="jumbotron rounded-border" id="content">
+   			<h3>Plus d'informations</h3>
+		</section>
+		<br style="clear: all;" />
+ 		<?php echo $dog_by_id->textdog ?>
+		</div>
 		</div>
 		<div class="col-lg-5">
 			<div class="dog-text">
