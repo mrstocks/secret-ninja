fe8b222bcc74fa6d83720d504288670769804aba
najela <achoarnold@live.com>
2014-02-04 02:14:17 +0100

    Design on Chiens section

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 error_reporting(E_ALL);
-ini_set('display_errors', 'On');
+ini_set('display_errors', 'Off');
 
 /**
  * Require my stupid config
--- views/chiens/show.php
+++ views/chiens/show.php
@@ -4,21 +4,25 @@
 <section class="jumbotron rounded-border" id="content">
     <h3><?php echo $dog_by_id->name ?></h3>
 </section>
-</div>
+
 <br style="clear: all">
-<div class="container">
 
 	<div class="row">
 		<div class="col-lg-7" style="padding-top: 15px;">
+			<dig  class="dog-carrousel">
+   			<?php $helper::carrousel($dog_by_id->dogimages, "dogimage"); ?>
+   		</dig>
 
-   		<?php $helper::carrousel($dog_by_id->dogimages, "dogimage"); ?>
-
-		 <br />
+   		<div class="dog-info">
+   		<h3>Plus d'informations</h3>
 			<?php echo $dog_by_id->textdog ?>
+			</div>
 		</div>
 		<div class="col-lg-5">
+			<div class="dog-text">
 			<p><?php echo $dog_by_id->textright ?></p>
-			
+		</div>
+
 		</div>
 	</div>
 </div>
