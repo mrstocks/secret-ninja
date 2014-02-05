4f5d8cee3b864b7b51036df5db0c9bbbec142012
najela <achoarnold@live.com>
2014-02-05 13:05:17 +0100

    Some patches

--- views/shared/footer.php
+++ views/shared/footer.php
@@ -32,7 +32,7 @@
         </div>
 
         <div class="col-md-3">
-          <div class="tile tile-hot">
+          <div class="tile">
             <?php echo helper::image_tag('/public/images/icons/png/Chat.png', 'title-image'); ?>
             <h3 class="tile-title">Social network</h3>
             <p>Nous sommes sociale! Venir nous voir sur facebook</p>
